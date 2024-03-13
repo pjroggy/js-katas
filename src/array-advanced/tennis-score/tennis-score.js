c2fa66ae5d587ee4b28da21037835514e0569143
Yoann FORTIN <yoann.fortin.dev@gmail.com>
2024-03-13 17:30:16 +0100

    Finished

--- src/array-advanced/tennis-score/tennis-score.js
+++ src/array-advanced/tennis-score/tennis-score.js
@@ -30,11 +30,93 @@ Add you own tests.
 
 // TODO add your code here
 
-// Begin of tests
-const assert = require("assert");
+/*
+Etape 0: Créer une fonction qui prend en paramètre un tableau de nombres et retourne une chaîne de caractère
+Etape 1: Créer les 2 joueurs
+Etape 2: Récupérer les scores
+Etape 3: Comparer les scores pour préparer l'affichage
+Etape 4: Afficher en fonction des scores
 
-assert.strictEqual(typeof getScore, "function");
-assert.strictEqual(getScore.length, 1);
+========================================================
+
+Etape 0: Créer une fonction qui prend en paramètre un tableau de nombres et retourne une chaîne de caractère ✅
+Etape 1: Créer 2 variables initialisée à 0 ✅
+Etape 2: J'utilise une boucle pour parcourir le tableau ✅
+Etape 3: Vérification du joueur qui a scoré, pour incrémenter son tableau de score ✅
+Etape 4: Je déclare une fonction pour affecter les scores de tennis en fonction du résultat du joueur ✅
+Etape 4-1: Transformer le nombre de points en score de tennis ✅
+Etape 4-2: Retourner le score transformé ✅
+Etape 5: J'utilise cette fonction pour transformer le score des 2 joueurs ✅
+Etape 5-1: On stocke le retour de la fonction dans les variables dédiées ✅
+Etape 6: Comparer les scores des deux joueurs
+Etape 6-1: Retourner la traduction en langage tennis en fonction du score
+*/
+
+function getscore (scoreArray) {
+   let scorePlayerOne = 0;
+   let scorePlayerTwo = 0;
+
+   for (let i =0; i < scoreArray.length; i++ ) {
+      if (scoreArray[i] === 1) {
+         scorePlayerOne++;
+      } else {
+         scorePlayerTwo++;
+      }
+   }
+
+   scorePlayerOne = tennisScore(scorePlayerOne);
+   scorePlayerTwo = tennisScore(scorePlayerTwo);
+
+   if (scorePlayerOne === "ad in" || scorePlayerTwo === "ad in") {
+      return "ad in"
+   } else if (scorePlayerOne === scorePlayerTwo) {
+      return "deuce"
+   } else {
+      return `${scorePlayerOne}-${scorePlayerTwo}`
+   }
+}
+
+function tennisScore (scorePlayer) {
+   switch (scorePlayer) {
+      case 0:
+         return "love";
+      case 1: 
+         return "15";
+      case 2: 
+         return "30";
+      case 3:
+         return "40";   
+      default:
+         return "ad in"
+   }
+}
+
+console.log(getscore([1, 1, 1]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Begin of tests
+// const assert = require("assert");
+
+// assert.strictEqual(typeof getScore, "function");
+// assert.strictEqual(getScore.length, 1);
 // TODO add your tests:
 
 // End of tests
