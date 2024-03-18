da71b68827a233204bc2b359b459053bcc4f7ea3
Yoann FORTIN <yoann.fortin.dev@gmail.com>
2024-03-18 17:18:34 +0100

    Correction express

--- src/array/sum-of-odds/sum-of-odds.js
+++ src/array/sum-of-odds/sum-of-odds.js
@@ -11,4 +11,28 @@ Example:
 
 // TODO add your code here
 
+/*
+1. Créer la fonction sum ✅
+2. Rajouter un paramètre à la fonction nommé numberArray ✅
+3. Créer une variable pour stocker l'addition des valeurs qui sont impaires, initialisée à 0 ✅
+4. Créer une condition pour vérifier la présence d'un tableau non vide ✅
+4bis. Créer la boucle pour parcourir le tableau ✅
+5. Créer une condition pour additionner les nombres s'ils sont impaires ✅
+6. Retourner la variable 
+*/
+
+function sum (numberArray) {
+   let result = 0;
+
+   if(numberArray !== null && numberArray.length > 0) {
+      for(const number of numberArray) {
+         if(number % 2 !== 0) {
+            result += number;
+         }
+      }
+   }
+   console.log(result);
+   return result;
+}
+
 module.exports = sum;
